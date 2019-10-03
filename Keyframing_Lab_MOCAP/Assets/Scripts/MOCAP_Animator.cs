6ff26b6f2b0fcf3df39fb7f08d5568d14a14dfa4
Wishmitha Mendis <wishmitha.14@cse.mrt.ac.lk>
2019-10-03 17:45:40 +0800

    Initiate animator code

---
+++ Keyframing_Lab_MOCAP/Assets/Scripts/MOCAP_Animator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MOCAP_Animator : MonoBehaviour
+{
+    public GameObject rigged_model;
+    public TextAsset mocap_data;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void readCSV()
+    {
+        string[] records = mocap_data.text.Split('\n');
+        for (int i = 1; i < records.Length; i++)
+        {
+            string[] fields = records[i].Split(',');
+            
+        }
+    }
+}
