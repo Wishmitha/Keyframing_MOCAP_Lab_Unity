accda5187755544df0db6d7b00d685756843ce1c
Wishmitha Mendis <wishmitha.14@cse.mrt.ac.lk>
2019-10-03 17:19:11 +0800

    Generate filtered MOCAP data

--- mocap_dat_gen.py
+++ mocap_dat_gen.py
@@ -1,11 +1,28 @@
 import numpy as np
+import pandas as pd
+import json
 
 mc_data = np.genfromtxt('Motion_Capture_Data.csv', delimiter=',', skip_header=5, usecols=(range(0,71)))
+mc_data_df = pd.read_csv('Motion_Capture_Data.csv', sep=',',header=[0,1], skiprows=3)
+mc_data_df = mc_data_df.drop([1])
 
-col_map = {'head_end':2, 'head':8, '*chest':65, 'hip':35, 'upper_arm_l':11, 'lower_arm_l':17, 'hand_l':23, 'upper_leg_l':29, 
-           'lower_leg_l':38, 'foot_l':44, 'foot_end_l':50, 'upper_arm_r':14, 'lower_arm_r':20, 'hand_r':26,c'upper_leg_r':32, 
-           'lower_leg_r':41, 'foot_r':47, 'foot_end_r':53}
+game_objects = ['Head_End','Head','Chest','Hip','Upper_Arm_L','Lower_Arm_L','Hand_L','Upper_Leg_L','Lower_Leg_L',
+                'Foot_L','Foot_End_L','Upper_Arm_R','Lower_Arm_R','Hand_R','Upper_Leg_R','Lower_Leg_R','Foot_R',
+                'Foot_End_R']
+markers = ['Top.Head','Rear.Head','R.Scapula','Sacral','L.Shoulder','L.Elbow','L.Wrist','L.ASIS','L.Knee','L.Ankle','L.Toe',
+           'R.Shoulder','R.Elbow','R.Wrist','R.ASIS','R.Knee','R.Ankle','R.Toe']
 
-cords = ['x','y','z']
+columns = ['Time'] + markers
+
+mc_data_df = mc_data_df[columns]
+
+mc_data_df.to_csv('mc_data_filtered.csv', index=False)
+
+object_marker_map = dict(zip(game_objects, markers))
+
+with open('object_marker_map.json', 'w') as fp:
+    json.dump(object_marker_map, fp)
 
 
+    
+    
