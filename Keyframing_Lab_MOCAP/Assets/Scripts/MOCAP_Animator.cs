ece38f6e5cf98353b092929a4a7a4c45419e63db
Wishmitha Mendis <wishmitha.14@cse.mrt.ac.lk>
2019-10-03 20:53:41 +0800

    Update folder structure

--- Keyframing_Lab_MOCAP/Assets/Scripts/MOCAP_Animator.cs
+++ Keyframing_Lab_MOCAP/Assets/Scripts/MOCAP_Animator.cs
@@ -5,7 +5,7 @@ using System.IO;
 
 public class MOCAP_Animator : MonoBehaviour
 {
-    public GameObject rigged_model;
+    public GameObject game_obj;
     public TextAsset mocap_data;
 
     // Start is called before the first frame update
@@ -17,16 +17,17 @@ public class MOCAP_Animator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void readCSV()
+    void Animate()
     {
         string[] records = mocap_data.text.Split('\n');
         for (int i = 1; i < records.Length; i++)
         {
             string[] fields = records[i].Split(',');
-            
+            //game_obj.transform.Rotate(float.Parse(fields[1]), float.Parse(fields[2]), float.Parse(fields[3]));
+            game_obj.transform.position = new Vector3(float.Parse(fields[1]), float.Parse(fields[2]), float.Parse(fields[3]));
         }
     }
 }
